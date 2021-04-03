166470dd917094c3d29ac838358089858032d185
Roberto-1998 <rcastellong98@gmail.com>
2021-04-03 12:25:22 -0400

    Creada función para retornar detalles de un cocktail por su id

--- src/app/services/cocktails.service.ts
+++ src/app/services/cocktails.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 
@@ -26,6 +26,12 @@ export class CocktailsService {
 
   getNonAlcoholicDrinks(){
     return this.http.get(`${this.URL}/api/json/v1/1/filter.php?a=Non_Alcoholic`).pipe(map((result:any)=>result.drinks));
+  }
+
+  getCocktailDetails(id:string){
+    let params=new HttpParams();
+    params=params.append('iid', id);
+    return this.http.get(`${this.URL}/api/json/v1/1/lookup.php${params}`);
   }
 
 
